c88bd6b2f95a899e33f2ac0aec442e8ca6d54fed
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-09-08 22:29:30 +0530

    Added: first name validation functionality

--- com.bl/src/UserRegistration.java
+++ com.bl/src/UserRegistration.java
@@ -4,6 +4,25 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration System");
+        Scanner scan = new Scanner(System.in);
+
+        UserRegistration registration = new UserRegistration();
+
+        System.out.println("Enter first name: ");
+        String firstName = scan.next();
+
+        registration.validFirstName(firstName);
+    }
+
+    private void validFirstName(String firstName) {
+        String regex = "^[A-Z][A-Za-z]{2,15}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.matches()) {
+            System.out.println("User first name is valid :) ");
+        }
+        else {
+            System.out.println("User first name is Invalid :( ");
+        }
     }
 }
