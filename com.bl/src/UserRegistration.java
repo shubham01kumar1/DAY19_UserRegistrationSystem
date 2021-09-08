464a7dc88ada0cd044de42ee4744ddeefe7d8aac
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-09-08 22:23:51 +0530

    Greeting message

---
+++ com.bl/src/UserRegistration.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration System");
+    }
+}
