5c0391a2096d1577ccf36504b9c89b0653b6a9cb
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-09-08 22:47:10 +0530

    Added: last name validation functionality

--- com.bl/src/UserRegistration.java
+++ com.bl/src/UserRegistration.java
@@ -10,8 +10,11 @@ public class UserRegistration {
 
         System.out.println("Enter first name: ");
         String firstName = scan.next();
-
         registration.validFirstName(firstName);
+
+        System.out.println("/n Enter last name: ");
+        String lastName = scan.next();
+        registration.validLastName(lastName);
     }
 
     private void validFirstName(String firstName) {
@@ -23,6 +26,17 @@ public class UserRegistration {
         }
         else {
             System.out.println("User first name is Invalid :( ");
+        }
+    }
+
+    private void validLastName(String lastName) {
+        String regex = "^[A-Z][A-Za-z]{2,15}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        if (matcher.matches()) {
+            System.out.println("User last name is valid :) ");
+        } else {
+            System.out.println("User last name is Invalid :( ");
         }
     }
 }
